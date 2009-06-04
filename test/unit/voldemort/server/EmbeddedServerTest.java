68befc83068f119f2d064d59a75abf684ebdb092
Mirko Caserta <mirko.caserta@gmail.com>
2009-06-04 23:07:52 +0200

    Made the server embeddable and configurable via code (metadata store no more needs to be persisted on the local filesystem)

---
+++ test/unit/voldemort/server/EmbeddedServerTest.java
@@ -0,0 +1,60 @@
+package voldemort.server;
+
+import junit.framework.TestCase;
+
+import java.util.Properties;
+import java.util.List;
+import java.util.ArrayList;
+
+import voldemort.cluster.Cluster;
+import voldemort.cluster.Node;
+import voldemort.store.StoreDefinition;
+import voldemort.store.Store;
+import voldemort.serialization.SerializerDefinition;
+import voldemort.client.*;
+import voldemort.routing.RoutingStrategy;
+import voldemort.routing.RoutingStrategyType;
+
+public class EmbeddedServerTest extends TestCase {
+
+    private final Properties configProperties = new Properties();
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        configProperties.put("node.id", "0");
+        configProperties.put("voldemort.home", "/tmp");
+        configProperties.put("bdb.cache.size", "50MB");
+
+        List<Integer> partitions = new ArrayList<Integer>();
+        partitions.add(0);
+        partitions.add(1);
+        Node node = new Node(0, "localhost", 8080, 6666, 8081, partitions);
+        List<Node> nodes = new ArrayList<Node>();
+        nodes.add(node);
+        Cluster cluster = new Cluster("testCluster", nodes);
+
+        List<StoreDefinition> storeDefs = new ArrayList<StoreDefinition>();
+        SerializerDefinition keySerializer = new SerializerDefinition("json", "\"string\"");
+        SerializerDefinition valSerializer = new SerializerDefinition("json", "\"string\"");
+        StoreDefinition storeDef = new StoreDefinition("test", "memory", keySerializer, valSerializer, RoutingTier.CLIENT, RoutingStrategyType.CONSISTENT_STRATEGY, 1, 1, 1, 1, 1, 1);
+        storeDefs.add(storeDef);
+
+        VoldemortConfig config = new VoldemortConfig(configProperties);
+        VoldemortMetadata metadata = new VoldemortMetadata(cluster, storeDefs, 0);
+
+        VoldemortServer server = new VoldemortServer(config, metadata);
+        server.start();
+    }
+
+    public void testEmbeddedServer() {
+        ClientConfig config = new ClientConfig();
+        config.setBootstrapUrls("tcp://localhost:6666");
+        StoreClientFactory storeClientFactory = new SocketStoreClientFactory(config);
+        StoreClient<String, String> storeClient = storeClientFactory.getStoreClient("test");
+        storeClient.put("foo", "bar");
+        String value = storeClient.getValue("foo");
+        assertEquals("bar", value);
+    }
+
+}
